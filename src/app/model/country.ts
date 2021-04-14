3cec86c50c0c7c63c62da80142c3c646fbe401e3
Guilherme C. Tonello <guitonello@outlook.com.br>
2021-04-14 10:43:58 -0300

    Adding model for country

---
+++ src/app/model/country.ts
@@ -0,0 +1,11 @@
+export class Country {
+    name: string;
+    capital: string;
+    alpha3Code: string;
+    region: string;
+    subregion: string;
+    population: number;
+    languages: string[] = [];
+    currencies: string[] = [];
+    flag: string;
+}
